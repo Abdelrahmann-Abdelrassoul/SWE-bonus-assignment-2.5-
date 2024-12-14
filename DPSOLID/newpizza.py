b5454f3759fd24ddbf93ef01c648d73cc51af82a
Abdelrahman Abdelrassoul <s-abdelrahman.abdelrassoul@zewailcity.edu.eg>
2024-12-14 21:17:14 +0200

    push dp

--- DPSOLID/newpizza.py
+++ DPSOLID/newpizza.py
@@ -9,6 +9,12 @@ class InventoryManager:
         "Olives": 10,
         "Mushrooms": 12,
     }
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(InventoryManager, cls).__new__(cls)
+
+        return cls._instance
     def check_and_decrement(self, item: str) -> bool:
         if self._inventory.get(item, 0) > 0:
             self._inventory[item] -= 1
